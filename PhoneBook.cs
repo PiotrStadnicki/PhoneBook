25c66643f3dcdda1689130c17be3ac6d29233350
Piotr Stadnicki <88381909+sfeniks@users.noreply.github.com>
2022-03-04 16:42:26 +0100

    Add files via upload

---
+++ PhoneBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_book
+{
+    class PhoneBook
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);        
+        }
+
+        public void DispalayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Dane kontaktowe : Imie {contact.Name}, numer : {contact.Number}");
+        }
+        public void DispalayContactsDetails(List <Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DispalayContactDetails(contact);
+            }
+
+        }
+
+        public void DispalayContact(string number)
+        {
+            var contact = Contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Podany kontakt nie znnajduje się w ksiązce adresowej");
+
+            }
+            else
+            {
+                DispalayContactDetails(contact);
+            }
+        }
+        public void DispalayAllContact()
+        {
+            DispalayContactsDetails(Contacts);
+
+        }
+        public void DisplayMatchingContacts(string searchPhrase)
+        {
+            var matchingContacts = Contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            DispalayContactsDetails(matchingContacts);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Phone_book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Książka telefoniczna ");
+            Console.WriteLine("1 Dodawanie kontaktów");
+            Console.WriteLine("2 Znajdz kontakt po numerze ");
+            Console.WriteLine("3 WYświetl całą liste kontaktów");
+            Console.WriteLine("4 wyszukowanie kontaktów ");
+            Console.WriteLine("x aby wyjść wciśniej   ");
+            var userInput = Console.ReadLine();
+            var phoneBook = new PhoneBook();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine(" Wprowadź numer");
+                        var number = Console.ReadLine();
+                        Console.WriteLine(" Wprowadź Imie");
+                        var name = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+                        phoneBook.AddContact(newContact);
+                        break;
+                    case "2":
+                        Console.WriteLine("Wprowadź numer");
+                        var numberToSearch = Console.ReadLine();
+                        phoneBook.DispalayContact(numberToSearch);
+                        break;
+                    case "3":
+                        phoneBook.DispalayAllContact();
+                        break;
+                    case "4":
+                        Console.WriteLine("Wpisz ywszukiwaną fraze");
+                        var searchPhrase = Console.ReadLine();
+                        phoneBook.DisplayMatchingContacts(searchPhrase);
+                        break;
+                    case "x":
+                        return;
+                    default:
+                        Console.WriteLine("niezrozumiała kompnda");
+                        break;
+                }
+                Console.WriteLine("Wybierz czynność");
+                userInput = Console.ReadLine();
+            }
+
+
+            
+
+        }
+    }
+}
